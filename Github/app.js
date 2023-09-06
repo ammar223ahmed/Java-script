988347801ec9f3b4ccaa1003a5266ba5f75cd00d
Ammar Ahmed <143522718+ammar223ahmed@users.noreply.github.com>
2023-09-06 22:37:04 +0500

    Add files via upload

---
+++ Github/app.js
@@ -0,0 +1,126 @@
+// Chapter no 1
+// Q1
+
+alert("Ammar")
+// Q2
+alert("Ahmed")
+// Q3
+alert("dirleader123@gmail.com")
+// Q4
+alert("03332336322")
+// Q5
+alert("Password")
+// Chapter  no 1 end
+
+// Chapter no 2 (Variables for string)
+// Q1
+var log  = "This is a camelCase variable";
+console.log(log);
+//0 Q2
+var log = "This is a string assignment"; 
+console.log(log)
+// Q3
+var teamName = "Your Team Name"; 
+alert(teamName); 
+
+// Q4
+var log= "John"; 
+console.log(log)
+// Chapter no 2 end
+
+// Chapter no 3 (Variables for numbers)
+// Q1
+var log= "caseQty";
+console.log(log)
+
+// Q2
+var log= "caseQty"; 
+caseQty =  144;
+console.log(log+caseQty)
+// Q3
+var  num = 9; 
+console.log(num)
+
+// Q4
+var result; 
+result = 5 + 3; 
+console.log(result)
+
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal); 
+
+// Q6
+
+var myNumber = 5; 
+myNumber = myNumber + 3; 
+
+console.log(myNumber); 
+
+// Chapter no 3 end
+
+
+// Chapter no 4 (Variable names Legal and Illegal)
+// Q1
+var  productCost = 3.45
+console.log(productCost);
+// Q2
+var nameOfBand;
+console.log(nameOfBand)
+// Q3
+var myNumber = 42; 
+console.log(myNumber);
+// Q4
+var firstName = "Ammar";
+var lastName = "Ahmed";
+var fullName = firstName + lastName; 
+console.log(fullName);
+// Q5
+// legal variables
+
+var $myVariable = " Learn Programing ";
+var myString= " This is My String ";
+var Str12 = " This is my number";
+console.log($myVariable+myString+Str12);
+// illegal name
+var num = " This is my Number";
+var myExlamation = " ! Sign";
+console.log(num+myExlamation);
+
+// Chapter no 4 end
+
+// Chapter no 5 (Math Expression I)
+// Q1
+var  remainder = 10 % 3; 
+console.log(remainder);
+
+// Q2
+var num = 20 % 6;
+console.log(num);
+
+// Q3
+var largeNum = 1000 * 2000; 
+console.log(largeNum);
+
+
+// Q4
+var firstNumber = 10;
+var secondNumber = 5;
+var result = firstNumber - secondNumber; 
+console.log(result);
+
+
+// Q5
+var remainder = 15 % 7; 
+console.log(remainder);
+
+
+// Q6
+var num1 = 5;
+var num2 = 10;
+var result = num1 * num2;
+
+alert("The result of the multiplication is: " + result);
